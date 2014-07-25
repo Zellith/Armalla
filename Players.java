feb08d4eaa7c0ea419da17033b841935da9c76b1
Zellith <Anarch.Balinario@gmail.com>
2014-07-25 20:46:24 +0800

    Players.java
    The class where the player name is stored in.

---
+++ Players.java
@@ -0,0 +1,17 @@
+
+public class Players {
+	
+	//fields
+	private String playerName;
+	
+	
+	public void setName(String name)
+	{
+		playerName = name;
+	}
+	
+	public String getName()
+	{
+		return playerName;
+	}
+}
